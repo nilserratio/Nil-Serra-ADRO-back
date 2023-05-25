237885aea24a76b5118e9b544d2283b55d3af309
nilserratio <nilserratio@gmail.com>
2023-05-25 18:18:50 +0200

    Create pingController controller test

---
+++ src/server/controllers/ping/pingController.test.ts
@@ -0,0 +1,28 @@
+import { type Request, type Response } from "express";
+import { pingController } from "./pingController.js";
+
+describe("Given a pingController controller", () => {
+  describe("When it receive a response", () => {
+    const req = {};
+    const res: Pick<Response, "status" | "json"> = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    test("Then it should invoke his status method with a 200", () => {
+      const expectedStatusCode = 200;
+
+      pingController(req as Request, res as Response);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+
+    test("Then it should invoke his json method with the message 'Entered'", () => {
+      const expectedMessage = "Entered";
+
+      pingController(req as Request, res as Response);
+
+      expect(res.json).toHaveBeenCalledWith({ message: expectedMessage });
+    });
+  });
+});
