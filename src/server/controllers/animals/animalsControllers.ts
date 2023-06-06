08d4344ece6733a7284422bac1372fd127c7c83f
nilserratio <nilserratio@gmail.com>
2023-06-06 22:24:06 +0200

    Create removeAnimal controlle

--- src/server/controllers/animals/animalsControllers.ts
+++ src/server/controllers/animals/animalsControllers.ts
@@ -1,5 +1,7 @@
 import { type Request, type NextFunction, type Response } from "express";
 import Animal from "../../../database/models/Animal.js";
+import CustomError from "../../../CustomError/CustomError.js";
+import { statusCode } from "../../utils/responseData/responseData.js";
 
 export const getAnimals = async (
   req: Request,
@@ -11,6 +13,30 @@ export const getAnimals = async (
 
     res.status(200).json({ animals });
   } catch (error) {
+    next(error);
+  }
+};
+
+export const removeAnimal = async (
+  req: Request<{ id: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.params;
+
+  try {
+    const animal = await Animal.findByIdAndDelete(id).exec();
+
+    if (!animal) {
+      const error = new CustomError(statusCode.notFound, "Animal not found");
+
+      throw error;
+    }
+
+    res
+      .status(200)
+      .json({ message: `Animal ${animal._id.toString()} removed` });
+  } catch (error: unknown) {
     next(error);
   }
 };
