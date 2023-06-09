fcf1131b42b38de430b1fb5aa37a5d4aa57e99ac
nilserratio <nilserratio@gmail.com>
2023-06-09 13:25:19 +0200

    Add createAnimalSchema Joi validation

---
+++ src/schemas/AnimalShemas.ts
@@ -0,0 +1,15 @@
+import { Joi } from "express-validation";
+import { type AnimalStructure } from "../types.js";
+
+export const createAnimalSchema = {
+  body: Joi.object<AnimalStructure>({
+    name: Joi.string().required(),
+    species: Joi.string().required(),
+    races: Joi.string().required(),
+    gender: Joi.string().required(),
+    size: Joi.string().required(),
+    yearOfBirth: Joi.string().required(),
+    imageUrl: Joi.string().required(),
+    description: Joi.string().required(),
+  }),
+};
