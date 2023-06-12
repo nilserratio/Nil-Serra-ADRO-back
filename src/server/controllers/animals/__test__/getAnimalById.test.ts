2eaa61d88839b79f1a696ddfece8bd0055844e4a
nilserratio <nilserratio@gmail.com>
2023-06-12 20:04:38 +0200

    Add getAnimalById controller test

--- src/server/controllers/animals/__test__/getAnimalById.test.ts
+++ src/server/controllers/animals/__test__/getAnimalById.test.ts
@@ -2,7 +2,7 @@ import { type Response } from "express";
 import { type CustomRequest } from "../../../types";
 import { animalsMock } from "../../../../mocks/animals/animalsMocks";
 import Animal from "../../../../database/models/Animal";
-import { removeAnimal } from "../animalsControllers";
+import { getAnimalById } from "../animalsControllers";
 import { statusCode } from "../../../utils/responseData/responseData";
 import CustomError from "../../../../CustomError/CustomError";
 
@@ -13,7 +13,7 @@ beforeEach(() => {
   jest.clearAllMocks();
 });
 
-describe("Given a removeAnimal controller", () => {
+describe("Given a getAnimalById controller", () => {
   const res: CustomResponse = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn(),
@@ -30,22 +30,20 @@ describe("Given a removeAnimal controller", () => {
   describe("When it recieve a response and the animal exist", () => {
     const animalMock = animalsMock[0];
 
-    Animal.findByIdAndDelete = jest
+    Animal.findById = jest
       .fn()
       .mockReturnValue({ exec: jest.fn().mockResolvedValue(animalMock) });
 
     test("Then it should call the response's method status code with 200", async () => {
-      await removeAnimal(req as CustomRequest, res as Response, next);
+      await getAnimalById(req as CustomRequest, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(statusCode.ok);
     });
 
-    test("Then it should call the response's method json with a message 'Animal removed'", async () => {
-      const expectedMessage = "Animal removed";
+    test("Then it should call the response's method json with the animal", async () => {
+      await getAnimalById(req as CustomRequest, res as Response, next);
 
-      await removeAnimal(req as CustomRequest, res as Response, next);
-
-      expect(res.json).toHaveBeenCalledWith({ message: expectedMessage });
+      expect(res.json).toHaveBeenCalledWith({ animalById: animalsMock[0] });
     });
   });
 
@@ -56,11 +54,11 @@ describe("Given a removeAnimal controller", () => {
         "Animal not found"
       );
 
-      Animal.findByIdAndDelete = jest.fn().mockReturnValue({
+      Animal.findById = jest.fn().mockReturnValue({
         exec: jest.fn().mockResolvedValue(null),
       });
 
-      await removeAnimal(req as CustomRequest, res as Response, next);
+      await getAnimalById(req as CustomRequest, res as Response, next);
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });
